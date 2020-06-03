b160cac4facebf1c5d3c2669c834012dd6872cb9
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-06-03 15:26:45 +0100

    implemented house listing page

---
+++ src/HouseList/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+import "./index.css";
+
+import House from "../House";
+
+class HouseList extends React.Component {
+  render() {
+    return (
+      <div className="house-list-wrapper">
+        <h2>House List</h2>
+        <Row gutter={[24, 16]}>
+          <Col sm={24} md={12} lg={6}>
+            <House name="Eyitayo's House" members={["A", "B", "C"]} />
+          </Col>
+          <Col sm={24} md={12} lg={6}>
+            <House name="Eyitayo's House" members={["A", "B", "C"]} />
+          </Col>
+          <Col sm={24} md={12} lg={6}>
+            <House name="Eyitayo's House" members={["A", "B", "C"]} />
+          </Col>
+          <Col sm={24} md={12} lg={6}>
+            <House name="Eyitayo's House" members={["A", "B", "C"]} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default HouseList;
